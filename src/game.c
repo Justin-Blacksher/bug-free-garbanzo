6201e2b693d0cf0adfc2fe1bc276ba16d0f6f611
jblacksher <j.redwatersec@gmail.com>
2025-07-01 22:36:01 -0700

    Initial commit and game.h coded. game.c Mostly coded. and Raylib.dll loaded.

---
+++ src/game.c
@@ -0,0 +1,143 @@
+#include "game.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void InitGame(Game *game) {
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Krazy Kingman Dopes");
+    SetTargetFPS(60);
+    game->camera = (Camera2D){0};
+    game->camera.zoom = 1.0f;
+    game->camera.offset = (Vector2){SCREEN_WIDTH/2.0f, SCREEN_HEIGHT/2.0f};
+
+    game->font = LoadFont("assets/ManufacturingConsent-Regular.ttf");
+    if (game->font.texture.id == 0) {
+        TraceLog(LOG_ERROR, "Failed to load font");
+        return;
+    }
+
+    game->map.tileset = LoadTexture("assets/tiles.png");
+    if (game->map.tileset.id == 0) {
+        TraceLog(LOG_ERROR, "Failed to load Tileset");
+        return;
+    }
+
+    game->player.x = 32;
+    game->player.y = 32;
+    game->player.hp = 100;
+    game->player.mp = 50;
+    game->player.virtue = 0;
+    game->player.item_count = 0;
+    for (int i = 0; i < MAX_ITEMS; i++) {
+        game->player.inventory[i] = NULL;
+    }
+
+    for (int y = 0; y < MAP_HEIGHT; y++) {
+        for (int x = 0; x < MAP_WIDTH; x++) {
+            if (x == 0 || x == MAP_WIDTH - 1 || y == 0 || y == MAP_HEIGHT - 1) {
+                game->map.tiles[x][y] = 2;
+                
+            } else if (rand() % 10 < 3) {
+                game->map.tiles[x][y] = 1;
+
+            } else {
+                game->map.tiles[x][y] = 0;
+            }
+        }
+    }
+    game->map.tiles[33][33] = 3;
+
+    InitNPCs(game);
+    game->in_combat = 0;
+    game->running = 1;
+    
+}
+
+void InitNPCs(Game *game) {
+
+    game->npcs[0].x = 34;
+    game->npcs[0].y = 32;
+    game->npcs[0].name = strdup("KrakHed");
+    game->npcs[0].dialogue = strdup("Jacked up honor! Not good for life");
+    game->npcs[0].active = 1;
+    game->npcs[0].hp = 0;
+
+    game->npcs[1].x = 36;
+    game->npcs[1].y = 32;
+    game->npcs[1].name = strdup("CEO");
+    game->npcs[1].dialogue = strdup("Do not give away your hard earned money!");
+    game->npcs[1].active = 1;
+    game->npcs[1].hp = 50;
+
+    game->npcs[2].x = 38;
+    game->npcs[2].y = 32;
+    game->npcs[2].name = strdup("DoorDasher");
+    game->npcs[2].dialogue = strdup("Fight me for your food punk!");
+    game->npcs[2].active = 1;
+    game->npcs[2].hp = 70;
+
+    for (int i = 2; i < MAX_NPCS; i++) {
+        game->npcs[i].active = 0;
+    }
+}
+
+void RenderDialogue(Game *game, const char *text) {
+    BeginDrawing();
+    ClearBackground(BLACK);
+    DrawTextEx(game->font, text, (Vector2){SCREEN_WIDTH/4.0f, SCREEN_HEIGHT-50.0f}, 16, 1, WHITE);
+    EndDrawing();
+    WaitTime(2.0);
+}
+
+void StartCombat(Game *game, int npc_index) {
+    game->in_combat = 1;
+    game->combat_npc_index = npc_index;
+    char msg[100];
+    snprintf(msg, 100, "Time for you to whack %s!", game->npcs[npc_index].name);
+    RenderDialogue(game, msg);
+}
+
+void RenderStatus(Game *game) {
+    char status[200];
+    snprintf(status, 200, "HP: %d MP: %d Virtue: %d Items: %d", game->player.hp,game->player.mp, game->player.virtue, game->player.item_count);
+    DrawTextEx(game->font, status, (Vector2){10, 10}, 16, 1, WHITE);
+}
+
+void HandleInput(Game *game) {
+    if (WindowShouldClose()) {
+        game->running = 0;
+        return;
+    }
+
+    if (game->in_combat) {
+        if (IsKeyPressed(KEY_A)) {
+            NPC* enemy = &game->npcs[game->combat_npc_index];
+            enemy->hp -= 10;
+            char msg[100];
+            snprintf(msg, 100, "BAM! You hit %s for 10 damage! HP: %d", enemy->name, enemy->hp);
+            RenderDialogue(game, msg);
+            if (enemy->hp <= 0) {
+                snprintf(msg, 100, "Its FU Thursday for %s", enemy->name);
+                RenderDialogue(game, msg);
+                enemy->active = 0;
+                game->in_combat = 0;
+                game->player.virtue--;
+            }else{
+                game->player.hp -= 5;
+                snprintf(msg, 100, "%s is doing the needful on your arse! -5 Dam", enemy->name);
+                RenderDialogue(game, msg);
+                if (game->player.hp <= 0) {
+                    RenderDialogue(game, "You sucked and you died");
+                    game->running = 0;
+                }
+            }
+        }
+    }else{
+        int new_x = game->player.x, new_y = game->player.y;
+        if (IsKeyPressed(KEY_UP)) new_y--;
+        if (IsKeyPressed(KEY_DOWN)) new_y++;
+        if (IsKeyPressed(KEY_LEFT)) new_x--;
+        if (IsKeyPressed(KEY_RIGHT)) new_x++;
+        if (IsKeyPressed(KEY_SPACE)) {}
+    }
+            
---
+++ src/game.h
@@ -0,0 +1,57 @@
+#ifndef GAME_H
+#define GAME_H
+#include "../bin/raylib.h"
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+#define TILE_SIZE 32
+#define MAP_WIDTH 64
+#define MAP_HEIGHT 64
+#define MAX_NPCS 10
+#define MAX_ITEMS 6
+
+typedef struct {
+    int x, y;
+    int hp, mp;
+    int virtue;
+    char* inventory[MAX_ITEMS];
+    int item_count;
+} Player;
+
+
+typedef struct {
+    int tiles[MAP_WIDTH][MAP_HEIGHT];
+    Texture2D tileset;
+} Map;
+
+
+typedef struct {
+    int x, y;
+    char* name;
+    char* dialogue;
+    int active;
+    int hp;
+} NPC;
+
+typedef struct {
+    Camera2D camera;
+    Font font;
+    Player player;
+    Map map;
+    NPC npcs[MAX_NPCS];
+    int running;
+    int in_combat;
+    int combat_npc,index;
+} Game;
+
+void InitGame(Game *game);
+void HandleInput(Game *game);
+void UpdateGame(Game *game);
+void RenderGame(Game *game);
+void CleanupGame(Game *game);
+void InitNPCs(Game *game);
+void RenderDialogue(Game *game, const char *text);
+void StartCombat(Game *game, int npc_index);
+void RenderStatus(Game *game);
+
+#endif
